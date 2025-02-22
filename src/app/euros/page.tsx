b1956c2ebf6e1a1ec3ee01bf1f6ac1de151ed908
angelurrutdev <angeljurrut@gmail.com>
2025-02-22 15:49:26 +0000

    add new page euros

---
+++ src/app/euros/page.tsx
@@ -0,0 +1,27 @@
+// app/page.tsx
+import Header from '@/components/Header'
+import HeaderToggle from '@/components/HeaderToggle'
+import Calculator from '@/components/Calculator'
+import { getCurrency } from '@/lib/index'
+
+// Configuración de revalidación: se actualizará cada 600 segundos = 10 minutos
+export const revalidate = 600
+
+export default async function Home() {
+  // Estas llamadas se ejecutan en el servidor
+  const setCurrencyDollar = await getCurrency('euro')
+  // Renderizamos el contenido
+  return (
+    <>
+      <Header>
+        <HeaderToggle
+          value={setCurrencyDollar.monitors.bcv.price}
+          sign={'€'}
+          value_datetime={setCurrencyDollar.monitors.bcv.last_update}
+        />
+      </Header>
+      <Calculator
+        setCurrencyDollar={setCurrencyDollar} />
+    </>
+  )
+}
