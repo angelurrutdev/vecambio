8012e02ce1ba726886cd511abe910c4e2e8688f2
angelurrutdev <angeljurrut@gmail.com>
2025-02-22 15:50:15 +0000

    delete props

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,6 @@ import Header from '@/components/Header'
 import HeaderToggle from '@/components/HeaderToggle'
 import Calculator from '@/components/Calculator'
 import { getCurrency } from '@/lib/index'
-import { tasa_dolar } from '@/lib/tasas'
 
 // Configuración de revalidación: se actualizará cada 600 segundos = 10 minutos
 export const revalidate = 600
@@ -11,8 +10,6 @@ export const revalidate = 600
 export default async function Home() {
   // Estas llamadas se ejecutan en el servidor
   const setCurrencyDollar = await getCurrency('dollar')
-  const rawTasaEnparalelo = await tasa_dolar('enparalelovzla')
-  const rawTasaCentral = await tasa_dolar('bcv')
 
   // Renderizamos el contenido
   return (
@@ -26,8 +23,6 @@ export default async function Home() {
       </Header>
       <Calculator
         setCurrencyDollar={setCurrencyDollar}
-        rawTasaEnparalelo={rawTasaEnparalelo}
-        rawTasaCentral={rawTasaCentral}
       />
     </>
   )
