654bf40e50e44b1eb9a0d2fac8c16f42ea4bcd68
angelurrutdev <angeljurrut@gmail.com>
2025-02-28 16:57:04 +0000

    New Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,56 +1,66 @@
 import React from 'react'
-import { Logo, EuroIcon, DollarIcon, CashIcon } from '../icons/icons'
-import Link from 'next/link'
 import { ModeToggle } from './ThemeToggle'
+import { Logo, EuroIcon, DollarIcon, CashIcon } from '../icons/icons'
 
-interface HeaderProps {
-	children?: React.ReactNode // 'children' ahora es opcional
+interface NavItem {
+	href: string
+	label: string
+	className?: string
+	icon?: React.ComponentType
 }
 
-function Header({ children }: HeaderProps) {
+export default function Header() {
+	const items: NavItem[] = [
+		{
+			href: '/',
+			label: 'Convertir Dolares',
+			icon: DollarIcon,
+		},
+		{
+			href: '/tasas-de-cambio-promedio-hoy',
+			label: 'Convertir Euros',
+			icon: EuroIcon,
+		},
+		{ href: '/bancos', label: 'Otras Tasas', icon: CashIcon },
+	]
+
 	return (
-		<header className='border-b py-2 md:py-2 ring-white/40 dark:ring-white dark:border-b-white/40 border-b-white/20 px-2 text-dusky flex flex-row justify-between items-center'>
-			<nav className='flex flex-row'>
-				<Link href='/' className='flex'>
-					<Logo />
-					<span className='flex self-center whitespace-nowrap text-lg font-medium tracking-tight text-neutral-900 dark:text-neutral-50'>
-						VeCambio
-					</span>
-				</Link>
-				<ul className='flex flex-row items-center justify-center md:justify-between mx-4'>
-					<li>
-						<Link
+		<nav className='sticky top-3 z-10 flex items-center justify-between gap-8 rounded-full border px-3 py-1.5 backdrop-blur-lg bg-secondary-white/25 dark:bg-primary/25 md:top-2 mx-auto w-full max-w-3xl md:px-0 dark:border-secondary'>
+			<div className='flex items-center gap-6'>
+				<div className='ml-3 flex items-center gap-3'>
+					<span>
+						<a
+							className='flex items-center gap-1 dark:text-white font-semibold'
 							href='/'
-							className='dark:text-white inline-flex items-center space-x-3 justify-center whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 disabled:pointer-events-none disabled:opacity-50 dark:focus-visible:ring-neutral-700 focus-visible:ring-neutral-500 border border-neutral-200 bg-white shadow-sm hover:bg-neutral-100 hover:text-neutral-900 dark:border-neutral-800 dark:bg-neutral-900 dark:hover:bg-neutral-800 dark:hover:text-neutral-50 dark:hover:border-neutral-700/50 h-9 px-4 py-2 group'
 						>
-							<DollarIcon aria-hidden='true' />
-							<span className='md:block hidden'>Convertir Dolares</span>
-						</Link>
-					</li>
-					<li>
-						<Link
-							href='/euros'
-							className=' mx-1 dark:text-white inline-flex items-center space-x-3 justify-center whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 disabled:pointer-events-none disabled:opacity-50 dark:focus-visible:ring-neutral-700 focus-visible:ring-neutral-500 border border-neutral-200 bg-white shadow-sm hover:bg-neutral-100 hover:text-neutral-900 dark:border-neutral-800 dark:bg-neutral-900 dark:hover:bg-neutral-800 dark:hover:text-neutral-50 dark:hover:border-neutral-700/50 h-9 px-4 py-2 group'
-						>
-							<EuroIcon aria-hidden='true' />
-							<span className='md:block hidden'>Convertir Euros</span>
-						</Link>
-					</li>
-					<li>
-						<Link
-							href='/tasas'
-							className=' dark:text-white inline-flex items-center space-x-3 justify-center whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 disabled:pointer-events-none disabled:opacity-50 dark:focus-visible:ring-neutral-700 focus-visible:ring-neutral-500 border border-neutral-200 bg-white shadow-sm hover:bg-neutral-100 hover:text-neutral-900 dark:border-neutral-800 dark:bg-neutral-900 dark:hover:bg-neutral-800 dark:hover:text-neutral-50 dark:hover:border-neutral-700/50 h-9 px-4 py-2 group lg:mr-[340px]'
-						>
-							<CashIcon aria-hidden='true' />
-							<span className='md:block hidden'>Otras Tasas</span>
-						</Link>
-					</li>
-				</ul>
-			</nav>
-			{children}
-			<ModeToggle />
-		</header>
+							<Logo />
+							VeCambio
+						</a>
+					</span>
+				</div>
+				<div className='mx-auto items-center justify-center border border-transparent md:flex md:gap-1'>
+					<nav className='relative z-10 flex max-w-max flex-1 items-center justify-center'>
+						<div className='relative'>
+							<ul className='group flex flex-1 list-none items-center justify-center space-x-1'>
+								{items.map(item => (
+									<li>
+										<a
+											className={`group inline-flex w-max items-center justify-center px-2 py-2 text-sm font-medium transition-colors hover:text-neutral-800 focus:bg-accent focus:text-neutral-800 focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-accent/50 h-9 rounded-full bg-transparent hover:bg-accent/50 gap-1 text-neutral-500 ring-transparent ring-2 dark:text-white ${item.className}`}
+											href={item.href}
+										>
+											{item.icon && <item.icon />}
+											{item.label}
+										</a>
+									</li>
+								))}
+							</ul>
+						</div>
+					</nav>
+				</div>
+			</div>
+			<div className='px-3'>
+				<ModeToggle />
+			</div>
+		</nav>
 	)
 }
-
-export default Header
